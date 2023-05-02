00f5f649d0ed9cb079e1ec0c462083b7861d56dd
wahyudindra <wahyudi.indra@glovory.com>
2023-05-02 17:18:59 +0700

    [CHG] modify table

--- prisma/migrations/20230502094837_init/migration.sql
+++ prisma/migrations/20230502094837_init/migration.sql
@@ -23,8 +23,8 @@ CREATE TABLE "Book" (
 -- CreateTable
 CREATE TABLE "Borrow" (
     "id" TEXT NOT NULL,
-    "bookId" TEXT NOT NULL,
-    "memberId" TEXT NOT NULL,
+    "bookCode" TEXT NOT NULL,
+    "memberCode" TEXT NOT NULL,
     "returnedAt" TIMESTAMP(3),
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -49,10 +49,10 @@ CREATE UNIQUE INDEX "Book_title_key" ON "Book"("title");
 CREATE UNIQUE INDEX "Penalty_borrowId_key" ON "Penalty"("borrowId");
 
 -- AddForeignKey
-ALTER TABLE "Borrow" ADD CONSTRAINT "Borrow_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book"("code") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Borrow" ADD CONSTRAINT "Borrow_bookCode_fkey" FOREIGN KEY ("bookCode") REFERENCES "Book"("code") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Borrow" ADD CONSTRAINT "Borrow_memberId_fkey" FOREIGN KEY ("memberId") REFERENCES "Member"("code") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Borrow" ADD CONSTRAINT "Borrow_memberCode_fkey" FOREIGN KEY ("memberCode") REFERENCES "Member"("code") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Penalty" ADD CONSTRAINT "Penalty_borrowId_fkey" FOREIGN KEY ("borrowId") REFERENCES "Borrow"("id") ON DELETE CASCADE ON UPDATE CASCADE;
